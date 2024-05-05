026cab98efed4ff38dc1bfb1d87d408c3f4efeea
kumarmanojgit <krmanoj72099@gmail.com>
2024-05-06 01:17:05 +0530

    install all npm package

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./component/Navbar"
+
+function App() {
+  return (
+    <>
+      <div>
+      <h1 className="text-3xl font-bold underline">hello world</h1>
+      <Navbar></Navbar>
+        </div>
+    </>
+  )
+}
+
+export default App
